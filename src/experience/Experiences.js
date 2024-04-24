386725ae7eb18a595cd404be871b8cfda2c7880f
naniitevez <nicole.tevez.97@gmail.com>
2024-04-24 20:44:33 -0300

    Agrego seccion experiencia

---
+++ src/experience/Experiences.js
@@ -0,0 +1,23 @@
+export const AvenidaExperiences = [
+  "Desarrollo y mantenimiento de eCommerce.",
+  "Optimización de experiencia de usuario.",
+  "Mantenimiento y mejora de código.",
+];
+
+export const ConvectaExpetiences = [
+  "Creación de una plataforma web intuitiva y visualmente atractiva para la búsqueda de propiedades.",
+  "Constante contacto y comunicación con el desarrollador backend para perfeccionar la API.",
+  "Integración de funcionalidades clave como filtros de búsqueda avanzados.",
+];
+
+export const SofttekExperiences = [
+  "Desarrollo de un proyecto para Android y IOS.",
+  "Se utilizó la tecnología de Ionic con React y Typescript. Con Redux como manejo de estado.",
+  "Desempeño sobresaliente en el desarrollo frontend y maquetado con Sass.",
+];
+
+export const FizzmodExperiences = [
+  "Desarrollo y mantenimiento de eCommerce con el framework VTEX-IO.",
+  "Desarrollo y mejora de aplicaciones “custom” con React y Typescript para potenciar el funcionamiento de aplicaciones ya existentes en VTEX-IO",
+  "Propuestas y actividades para mejorar el funcionamiento, organización y comunicación del equipo.",
+];
